3bf94484497ed2245dd804b27ae5a4f3ff9ad409
maxrchung <maxrchung@gmail.com>
2021-12-25 19:16:57 -0800

    deploy s3 bucket

---
+++ lib/maxrchung-rails-cdk-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+// import * as sqs from '@aws-cdk/aws-sqs';
+
+export class MaxrchungRailsCdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'maxrchung-rails', {
+      bucketName: 'maxrchung-rails'
+    });
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'MaxrchungRailsCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
