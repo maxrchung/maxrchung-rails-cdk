128870d56e6c110a383535901f6bb4908523a66f
maxrchung <maxrchung@gmail.com>
2024-02-08 12:22:15 -0800

    add cloudfront

--- bin/maxrchung-rails-cdk.ts
+++ bin/maxrchung-rails-cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
-import * as cdk from '@aws-cdk/core'
+import * as cdk from 'aws-cdk-lib'
 import { MaxrchungRailsCdkStack } from '../lib/maxrchung-rails-cdk-stack'
 
 const app = new cdk.App()
@@ -10,6 +10,7 @@ new MaxrchungRailsCdkStack(app, 'MaxrchungRailsCdkStack', {
    * but a single synthesized template can be deployed anywhere. */
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
--- lib/maxrchung-rails-cdk-stack.ts
+++ lib/maxrchung-rails-cdk-stack.ts
@@ -1,13 +1,26 @@
-import * as cdk from '@aws-cdk/core'
-import * as s3 from '@aws-cdk/aws-s3'
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
 
 export class MaxrchungRailsCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     const bucket = new s3.Bucket(this, 'maxrchung-rails', {
       bucketName: 'maxrchung-rails'
     })
-    bucket.grantPublicAccess()
+
+    new cloudfront.Distribution(this, 'maxrchung-cloudfront', {
+      // Note: I'd like to set up a friendly domain just through here but this
+      // requires creating a certificate through ACM. I don't think it's worth
+      // the effort to go that far so I'm manually just adding a CNAME record
+      // in Route53 to map a s3.maxrchung.com domain to Cloudfront's distribution,
+      // something like d111111abcdef8.cloudfront.net. For more info:
+      // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/CNAMEs.html
+      // domainNames: ["s3.maxrchung.com"]
+      defaultBehavior: { origin: new origins.S3Origin(bucket) }
+    })
   }
 }
